e3c008f593bdd03c77eb0169df0c28d69d8c7d27
Adrien Masson <adrien.masson@hotmail.fr>
2023-01-14 23:06:13 +0100

    [DEV] Zustand Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useBearStore } from "./store/store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { bears, increase, reset } = useBearStore();
+
+	return (
+		<div className='App'>
+			<div>There are {bears} bears</div>
+			<button onClick={() => increase(1)}>One Up</button>
+			<button onClick={() => reset()}>Reset</button>
+		</div>
+	);
 }
 
 export default App;
